ade525563561a1cda17e574918284ee5bd2dd2ca
stylezzm <285301519@qq.com>
2022-11-21 15:36:11 +0800

    修改初始化文件

---
+++ js/chunk-2d0f0635.59a2c265.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0f0635"],{"9bc9":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("router-view")},c=[],r={name:"blank-view",computed:{},data(){return{}},watch:{}},u=r,o=t("2877"),w=Object(o["a"])(u,a,c,!1,null,"866b6422",null);n["default"]=w.exports}}]);
